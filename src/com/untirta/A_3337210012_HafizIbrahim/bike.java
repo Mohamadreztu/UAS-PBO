0c2a5793a84d0f7c0cd34aff708e198e33dbdc4c
Mohamad Restu Zikri Novdian <112332492+Mohamadreztu@users.noreply.github.com>
2023-06-12 19:13:35 +0700

    Update bike.java

--- src/com/untirta/A_3337210012_HafizIbrahim/bike.java
+++ src/com/untirta/A_3337210012_HafizIbrahim/bike.java
@@ -1,30 +1,26 @@
 package com.untirta.A_3337210012_HafizIbrahim;
 
-public class bike extends vehicle {
-    private int numOfWheels;
-    private int getSpeedLimit;
-    
-    public bike(String brand, int numOfWheels, int getSpeedLimit) {
+public final class bike extends vehicle {
+    private final int numOfWheels;
+    private final int speedLimit;
+
+    public bike(String brand, int numOfWheels, int speedLimit) {
         super(brand);
         this.numOfWheels = numOfWheels;
-        this.getSpeedLimit = getSpeedLimit;
+        this.speedLimit = speedLimit;
     }
-    
+
     public int getNumOfWheels() {
         return numOfWheels;
     }
-    
-    public void setNumOfWheels(int numOfWheels) {
-        this.numOfWheels = numOfWheels;
-    }
-    
+
     public void ride() {
         System.out.println("Riding the bike with " + getNumOfWheels() + " wheels.");
-        System.out.println("Speed Limit is " + getSpeedLimit() + "Km/s \n");
+        System.out.println("Speed Limit is " + speedLimit + " Km/s \n");
     }
 
     @Override
-    int getSpeedLimit(){
-        return getSpeedLimit;
+    int getSpeedLimit() {
+        return speedLimit;
     }
 }
