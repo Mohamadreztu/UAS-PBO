5bd6ab21b5789f5fd8eead759d10ab3ce91e1cad
Mohamad Restu Zikri Novdian <112332492+Mohamadreztu@users.noreply.github.com>
2023-06-12 19:19:28 +0700

    Update test.java

--- src/com/untirta/A_3337210012_HafizIbrahim/test.java
+++ src/com/untirta/A_3337210012_HafizIbrahim/test.java
@@ -2,13 +2,17 @@ package com.untirta.A_3337210012_HafizIbrahim;
 
 public class test {
     public static void main(String[] args) {
-        bike myBike = new bike("Honda", 2, 160);
-        car myCar = new car("Toyota", 4, 240);
+        vehicle myBike = new bike("Honda", 2, 160);
+        vehicle myCar = new car("Toyota", 4, 240);
         
         System.out.println("My bike brand is " + myBike.getBrand());
-        myBike.ride();
+        if (myBike instanceof bike) {
+            ((bike) myBike).ride();
+        }
         
         System.out.println("My car brand is " + myCar.getBrand());
-        myCar.drive();
+        if (myCar instanceof car) {
+            ((car) myCar).drive();
+        }
     }
 }
